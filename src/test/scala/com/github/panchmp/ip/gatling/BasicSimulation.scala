a5b75f2d107e7c2e8a2cd49e645f51f6548141e6
Michael Panchenko <panchmp@gmail.com>
2020-03-26 16:44:05 -0400

    Initial MVP  - add gatling performance tests

---
+++ src/test/scala/com/github/panchmp/ip/gatling/BasicSimulation.scala
@@ -0,0 +1,34 @@
+package com.github.panchmp.ip.gatling
+
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+
+class BasicSimulation extends Simulation {
+
+  private val httpProtocol = http
+    .baseUrl("http://localhost:8080/api/ip/")
+    .acceptHeader("application/json")
+    .doNotTrackHeader("1")
+
+  private val feeder = csv("data/ip.zip").unzip.random.circular
+
+  private val scn: ScenarioBuilder = scenario("BasicSimulation")
+    .feed(feeder)
+    .exec(
+      http("get_api")
+        .get("${ip}"))
+    .pause(100 millisecond)
+
+  setUp(
+    scn.inject(
+//      rampUsers(10) during (10 seconds), // 3
+//      constantUsersPerSec(10) during (20 seconds), // 4
+      rampConcurrentUsers(1) to (50) during (10 seconds),
+      constantConcurrentUsers(50) during (60 seconds)
+    )
+  ).protocols(httpProtocol)
+}
