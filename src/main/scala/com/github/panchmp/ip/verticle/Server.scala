d4235ce15a7e63abbf3715d654c885d23966550d
Michael Panchenko <panchmp@gmail.com>
2020-11-30 01:38:13 -0500

    Add health check

--- src/main/scala/com/github/panchmp/ip/verticle/Server.scala
+++ src/main/scala/com/github/panchmp/ip/verticle/Server.scala
@@ -1,12 +1,12 @@
 package com.github.panchmp.ip.verticle
 
-import io.vertx.core.http.HttpHeaders
+import io.vertx.core.http.{HttpHeaders, HttpMethod}
 import io.vertx.ext.web.{RoutingContext => JRoutingContext}
 import io.vertx.lang.scala.ScalaVerticle
 import io.vertx.micrometer.PrometheusScrapingHandler
 import io.vertx.scala.core.eventbus.Message
-import io.vertx.scala.ext.web.Router
 import io.vertx.scala.ext.web.handler.ErrorHandler
+import io.vertx.scala.ext.web.{Router, RoutingContext}
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.Future
@@ -20,27 +20,20 @@ class Server extends ScalaVerticle {
     val server = vertx.createHttpServer()
     val router: Router = Router.router(vertx)
 
-    router.get("/api/ip/:ip").handler(ctx => {
-      val ip = ctx.request().getParam("ip")
-      vertx.eventBus().sendFuture[String]("maxmind/ip", ip).onComplete {
-        case Success(msg: Message[String]) =>
-          ctx.response()
-            .putHeader(HttpHeaders.CONTENT_TYPE.toString, "application/json; charset=utf-8")
-            .end(msg.body())
-        case Failure(ex) =>
-          log.error("Can't process response for ip:" + ip, ex)
-          ctx.response()
-            .setStatusCode(500)
-            .setStatusMessage(ex.getMessage)
-            .end()
-      }
-    })
+    router.route.failureHandler(ErrorHandler.create(true))
+
+    router.route(HttpMethod.GET, "/healthz").handler(ctx =>
+      ctx.response.putHeader(HttpHeaders.CONTENT_TYPE.toString, "text/plain").end("OK")
+    )
 
     router.route("/metrics").handler(e => {
       PrometheusScrapingHandler.create.handle(e.asJava.asInstanceOf[JRoutingContext])
     })
 
-    router.route.failureHandler(ErrorHandler.create(true))
+    val apiRouter: Router = Router.router(vertx)
+    apiRouter.get("/ip/:ip").handler(getByIp)
+
+    router.mountSubRouter("/api/v1", apiRouter)
 
     val port = config.getInteger("server.http.port", 8080)
     server
@@ -48,4 +41,21 @@ class Server extends ScalaVerticle {
       .listenFuture(port)
       .map(_ => ())
   }
+
+  def getByIp(ctx: RoutingContext) = {
+    val ip = ctx.request().getParam("ip")
+    vertx.eventBus().sendFuture[String]("maxmind/ip", ip).onComplete {
+      case Success(msg: Message[String]) =>
+        ctx.response()
+          .putHeader(HttpHeaders.CONTENT_TYPE.toString, "application/json; charset=utf-8")
+          .end(msg.body())
+      case Failure(ex) =>
+        log.error("Can't process response for ip:" + ip, ex)
+        ctx.response()
+          .setStatusCode(500)
+          .setStatusMessage(ex.getMessage)
+          .end()
+    }
+  }
+
 }
--- src/test/scala/com/github/panchmp/ip/gatling/BasicSimulation.scala
+++ src/test/scala/com/github/panchmp/ip/gatling/BasicSimulation.scala
@@ -10,25 +10,25 @@ import scala.language.postfixOps
 class BasicSimulation extends Simulation {
 
   private val httpProtocol = http
-    .baseUrl("http://localhost:8080/api/ip/")
+    .baseUrl("http://localhost:8080")
     .acceptHeader("application/json")
     .doNotTrackHeader("1")
 
-  private val feeder = csv("data/ip.zip").batch.random
+  private val feeder = csv("data/ip.zip").unzip.batch.random
 
   private val scn: ScenarioBuilder = scenario("BasicSimulation")
     .feed(feeder)
     .exec(
       http("get_api")
-        .get("${ip}"))
+        .get("/api/v1/ip/${ip}"))
     .pause(50 millisecond)
 
   setUp(
     scn.inject(
       //rampUsers(10) during (10 seconds), // 3
       //constantUsersPerSec(10) during (20 seconds), // 4
-      rampConcurrentUsers(1) to (5) during (60 seconds),
-      constantConcurrentUsers(5) during (10 minutes)
+      rampConcurrentUsers(1) to (50) during (60 seconds),
+      constantConcurrentUsers(50) during (5 minutes)
     )
   ).protocols(httpProtocol)
 }
